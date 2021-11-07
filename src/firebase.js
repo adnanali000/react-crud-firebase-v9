4e10889223cce9b4717c1fccd71bb6e58f340f48
adnanali000 <adnan.museel123@gmail.com>
2021-11-07 11:33:30 +0500

    react crud firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+require('dotenv').config();
+
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: "react-crud-66f0f.firebaseapp.com",
+    projectId: "react-crud-66f0f",
+    storageBucket: "react-crud-66f0f.appspot.com",
+    messagingSenderId: "409563616666",
+    appId: "1:409563616666:web:07fb3ccf5baa2de0f55623"
+  };
+  
